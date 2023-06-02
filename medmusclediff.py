9cc161fa0407cf3553ad5fd4b97f167dd6d0fcb6
paulopreto <paulosantiago@usp.br>
2023-06-02 15:33:19 -0300

    grafico de barras

---
+++ medmusclediff.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('./results/medidas_retofemoral.csv', skiprows=1, delimiter=',', dtype='float')
+print(data)
+
+antes = data[0]
+depois = data[1]
+
+evolucaomuscle = depois - antes
+print(evolucaomuscle)
+
+plt.bar(['antes','depois'],[antes,depois])
+plt.show()
